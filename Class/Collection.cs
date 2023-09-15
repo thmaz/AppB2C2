2c3c946c7c1ffeefbeac68d1711136dd652d449e
Thomas Palmen <121097637+thmaz@users.noreply.github.com>
2023-09-15 10:04:05 +0200

    Add project files.

---
+++ Class/Collection.cs
@@ -0,0 +1,17 @@
+﻿namespace AppB2C2.Class
+{
+	public class Collection
+	{
+		public Array? items { get; set; }
+		public string collectionTitle { get; set; }
+		public string collectionDescription { get; set; }
+		public int itemsCount { get; set; }
+		public int dateCreated { get; set; }
+
+		public void updateCollection()
+		{
+			
+		}
+
+	}
+}
---
+++ Class/User.cs
@@ -0,0 +1,22 @@
+﻿namespace AppB2C2.Class
+{
+	public class User
+	{
+		public string userName {  get; set; }
+		public string userPassword { get; set; }
+		public string mailAdress { get; set; }
+
+		public Array? createdCollections { get; set; }
+		public Array? favoriteItems { get; set; }
+
+		public void createCollection()
+		{
+	
+		}
+
+		public void	addFavorite()
+		{
+
+		}
+	}
+}
