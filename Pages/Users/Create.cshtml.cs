23d4df5424f5e6122c72647bf18370a38118e407
Thomas Palmen <121097637+thmaz@users.noreply.github.com>
2023-10-03 14:54:27 +0200

    added bootswatch theme

---
+++ Pages/Users/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AppB2C2.Models;
+
+namespace AppB2C2.Pages.Users
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppB2C2.Models.AppDBContext _context;
+
+        public CreateModel(AppB2C2.Models.AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public User User { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Users == null || User == null)
+            {
+                return Page();
+            }
+
+            _context.Users.Add(User);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Users/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AppB2C2.Models;
+
+namespace AppB2C2.Pages.Users
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly AppB2C2.Models.AppDBContext _context;
+
+        public DetailsModel(AppB2C2.Models.AppDBContext context)
+        {
+            _context = context;
+        }
+
+      public User User { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(m => m.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                User = user;
+            }
+            return Page();
+        }
+    }
+}
